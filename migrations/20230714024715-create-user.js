dc6286a55c8b0388e489080d6c917754ae633d9f
Ckai <ckai.trans@gmail.com>
2023-07-14 14:35:20 +0800

    add user model

---
+++ migrations/20230714024715-create-user.js
@@ -0,0 +1,42 @@
+"use strict";
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("Users", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      email: {
+        type: Sequelize.STRING,
+      },
+      password: {
+        type: Sequelize.STRING,
+      },
+      role: {
+        // type: Sequelize.STRING,
+        type: Sequelize.ENUM,
+        values: ["buyer", "seller", "admin"],
+      },
+      buyer_phone: {
+        type: Sequelize.STRING,
+      },
+      shipping_address: {
+        type: Sequelize.STRING,
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable("Users");
+  },
+};
