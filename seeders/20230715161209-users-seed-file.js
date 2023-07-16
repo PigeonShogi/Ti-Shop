29e0fb5eaac1db858cee64cc8419bde67a0ead24
Ckai <ckai.trans@gmail.com>
2023-07-16 13:38:08 +0800

    edit users-seed-file for password encryption

--- seeders/20230715161209-users-seed-file.js
+++ seeders/20230715161209-users-seed-file.js
@@ -1,25 +1,32 @@
 "use strict";
+
+const bcrypt = require("bcrypt");
 const userData = require("./users.json");
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.bulkInsert(
-      "Users",
-      Array.from(userData, (value) => ({
-        email: value.email,
-        password: value.password,
-        role: value.role,
-        buyer_phone: value.buyer_phone,
-        shipping_address: value.shipping_address,
-        created_at: new Date(),
-        updated_at: new Date(),
-      })),
-      {}
-    );
+    // userData 的密碼需加密處理
+    for (const user of userData) {
+      const salt = await bcrypt.genSalt(10);
+      const hash = await bcrypt.hash(user.password, salt);
+      user.password = hash;
+    }
+
+    const userBulkData = Array.from(userData, (value) => ({
+      email: value.email,
+      password: value.password,
+      role: value.role,
+      buyer_phone: value.buyer_phone,
+      shipping_address: value.shipping_address,
+      created_at: new Date(),
+      updated_at: new Date(),
+    }));
+
+    await queryInterface.bulkInsert("Users", userBulkData, {});
   },
 
   async down(queryInterface, Sequelize) {
-    await queryInterface.bulkDelete("People", null, {});
+    await queryInterface.bulkDelete("Users", null, {});
   },
 };
--- seeders/20230715161212-orders-seed-file.js
+++ seeders/20230715161212-orders-seed-file.js
@@ -35,6 +35,6 @@ module.exports = {
   },
 
   async down(queryInterface, Sequelize) {
-    await queryInterface.bulkDelete("People", null, {});
+    await queryInterface.bulkDelete("Orders", null, {});
   },
 };
